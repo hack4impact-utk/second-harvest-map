f9400c42689fa7f0a3409f407ea9c6ffa89ff47a
Reagan (Alex) Austin <71673490+raustin9@users.noreply.github.com>
2021-02-24 19:09:33 -0500

    Created food pantry type (#3)

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,23 @@
 export interface FoodPantry {
   name: string;
-  /*
-    Rest of fields
-  */
+  address: {
+    streetNumber: number;
+    streetName: string;
+    zipCode: number;
+  };
+  hours: {
+    monday: string[];
+    tuesday: string[];
+    wednesday: string[];
+    thursday: string[];
+    friday: string[];
+    saturday: string[];
+    sunday: string[];
+  };
+  phoneNumber: string;
+  position: {
+    latitude: number;
+    longitude: number;
+  };
+  website?: string;
 }
