6e44e8eeda030baecfa8e391d874175ba39e2468
braxtonhaynie <bhaynie@vols.utk.edu>
2021-02-24 19:37:35 -0500

    changed up getting location and removed promise

--- src/client/geolocation.ts
+++ src/client/geolocation.ts
@@ -1,41 +1,16 @@
-// To get location call geoLocation()
-// returns GeolocationCoordinates type
-// Usage var loc = geoLocation();
-// loc.latitude, loc.longitude, loc.altitude
-// See https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates for other location options
+// Geolocation returns the coordinates of a person's location
+// to access variables use location.latitude, location.longitude
 
-export var options = {
-  // tracking settings
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
-function position() {
-  const pos = new Promise((res, rej) => {
-    navigator.geolocation.getCurrentPosition(res, rej, options);
-  });
-  pos.then((res) => {
-    console.log('got location');
-  });
-  pos.catch((err) => {
-    // if there is an error or user denys location access
-    console.log('I get called:', err.message);
+export default function geoLocation(): GeolocationCoordinates {
+  // position returns geolocationposition
+  let location: GeolocationCoordinates | null = null;
+  navigator.geolocation.getCurrentPosition((pos: GeolocationPosition) => {
+    location = pos.coords;
   });
 
-  return pos;
-}
-
-export async function geoLocation() {
-  var geo: any;
-  try {
-    // position returns geolocationposition
-    geo = await position();
-
-    //just return coordinates
-    return geo.coords;
+  if (location == null) {
+    throw new Error('location was null');
   }
-  catch(err){
-    console.log(err);
-  }
+
+  return location;
 }
