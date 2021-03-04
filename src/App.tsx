5bedb8c6eadf7d31b055c1fadcbf4d4b24168868
Rizwaan <rizraak@hotmail.com>
2021-03-03 19:11:15 -0500

    Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <form>
-          <input type="text" />
-          <button type="button">Current Location</button>
-        </form>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react';
+
+const SearchButton: FunctionComponent = () => {
+    return (
+        <div>
+            <input type="text"/>
+            <button type="button">Current Location</button>
+        </div>
+    );
+};
+
+export default SearchButton;
