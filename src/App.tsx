c813d2558680ecad404657dca1630c73066281d8
Rizwaan <rizraak@hotmail.com>
2021-03-03 19:00:06 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,10 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <form>
+          <input type="text" />
+          <button type="button">Current Location</button>
+        </form>
       </header>
     </div>
   );
