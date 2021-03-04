593da039823b43fd4ba281698323cf01b428a86f
Rizwaan <rizraak@hotmail.com>
2021-03-03 19:12:44 -0500

    React Fragment

--- src/components/SearchButton/index.tsx
+++ src/components/SearchButton/index.tsx
@@ -2,10 +2,10 @@ import React, {FunctionComponent} from 'react';
 
 const SearchButton: FunctionComponent = () => {
     return (
-        <div>
+        <>
             <input type="text"/>
             <button type="button">Current Location</button>
-        </div>
+        </>
     );
 };
 
