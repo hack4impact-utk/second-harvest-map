2d9a1026c6d3f6764d8a530ebaedbdf9e74af84c
Riz-waan <rizraak@hotmail.com>
2021-03-10 19:04:03 -0500

    H4ISH-9 text field and button (#10)
    * test
    
    * Button Component
    
    * React Fragment
    
    * formatting fix
    
    Co-authored-by: Vishal Aiely <vaiely@vols.utk.edu>

---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+const SearchButton: FunctionComponent = () => {
+  return (
+    <>
+      <input type="text" />
+      <button type="button">Current Location</button>
+    </>
+  );
+};
+
+export default SearchButton;
