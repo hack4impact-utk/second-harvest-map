3515f4beba04003c35e726bb7bd9886c1b33de90
Kuba Żeligowski <k.zeligowski@gmail.com>
2021-03-17 16:16:57 -0400

    Imported FoodPantryContainer and Pagination, added props

--- src/components/FoodPantryDisplay/index.tsx
+++ src/components/FoodPantryDisplay/index.tsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { FoodPantry } from 'utils/types'
+import FoodPantryContainer from 'src/components/FoodPantryContainer/index'
+import { Pagination } from '@material-ui/lab'
 
-type CardDisplayState = {
-  pantries: FoodPantry[],
+interface Props {
+  pantries: FoodPantry[]
+}
+
+interface CardDisplayState {
   offset: number
 }
 
-class FoodPantryDisplay extends Component<{}, CardDisplayState> {
+class FoodPantryDisplay extends Component<Props, CardDisplayState> {
   render() {
     return (
       <div>
-        FoodPantryDisplay
+        <FoodPantryContainer pantries={this.props.pantries} />
+        <Pagination />
       </div>
     )
   }
 }
 
-export default FoodPantryDisplay;
+export default FoodPantryDisplay
