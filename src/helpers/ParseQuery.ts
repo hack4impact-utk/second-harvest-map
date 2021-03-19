3c3e9b5beca56f3b7483a20f84e940a95fd4eb85
Sai Thatigotla <sai.thatigotla@gmail.com>
2021-03-19 19:40:18 -0400

    Saithat/h4 ish 8 parse query (#13)
    * zip code testing works, others still failing test cases
    
    * passed current test cases, but prob need more
    
    * fixed linter errors
    
    * Forgot to run linter #13
    
    Co-authored-by: VishalAiely <vaiely@vols.utk.edu>

---
+++ src/helpers/ParseQuery.ts
@@ -0,0 +1,24 @@
+/**
+ * Returns what the input is (zip code, county name, or address)
+ * @Param str The string to parse
+ * @Returns A string telling what the input is
+ */
+export default function Parse(str: string): string {
+  const zip = new RegExp('^\\d{5}$');
+  // let county = new RegExp('(.+), ([a-z]{2})');
+  // let county: RegExp = /(.+), ([a-z]{2})/;
+  const county = new RegExp('(^[A-z]+), ([A-z]{2})');
+  const add1 = new RegExp('(.+), ([A-z]{2})');
+  // let add2: RegExp = /\d{1,6}\s(?:[A-Za-z0-9#]+\s){0,7}(?:[A-Za-z0-9#]+,)\s*(?:[A-Za-z]+\s){0,3}(?:[A-Za-z]+,)\s*[A-Z]{2}\s*\d{5}/;
+
+  if (zip.test(str)) {
+    return 'zip';
+  }
+  if (county.test(str)) {
+    return 'county';
+  }
+  if (add1.test(str)) {
+    return 'add1';
+  }
+  return 'error';
+}
