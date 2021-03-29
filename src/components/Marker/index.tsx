b84dbe2040feab66f16cd7f5882a9bfeae32270a
VishalAiely <vaiely@vols.utk.edu>
2021-03-29 16:37:59 -0400

    removed unused import and supressed unnecessary errors

--- src/components/Marker/index.tsx
+++ src/components/Marker/index.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { ChildComponentProps } from 'google-map-react';
 import './styles.css';
-import Pin from './pin.svg';
 
 const Marker: FunctionComponent<ChildComponentProps> = (): JSX.Element => {
   return (
