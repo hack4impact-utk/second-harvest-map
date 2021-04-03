f00752e48619e5551a08d2473b4da23f05188537
VishalAiely <vaiely@vols.utk.edu>
2021-04-03 14:07:20 -0400

    added get FoodPantries route

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,12 @@
 /* eslint-disable no-console */
-import express from 'express';
+import express, { Request, Response } from 'express';
+import getFoodPantries from './Contentful';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/foodPantries', async (req: Request, res: Response) => {
+  res.send(await getFoodPantries());
+});
