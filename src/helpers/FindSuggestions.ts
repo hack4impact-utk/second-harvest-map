8f7b54c42a5fa8ff61c1688a06b520a6636459df
VishalAiely <vaiely@vols.utk.edu>
2021-04-07 15:18:18 -0400

    removed unncessary file

--- src/helpers/FindSuggestions.ts
+++
@@ -1,9 +0,0 @@
-import { FoodPantry } from 'src/utils/types';
-
-const hasTextMatch = (textQuery: string, pantry: FoodPantry): boolean => {
-  return Boolean(
-    pantry.name.search(textQuery) || pantry.address.streetName.search(textQuery) || pantry.county.search(textQuery)
-  );
-};
-
-export default hasTextMatch;
