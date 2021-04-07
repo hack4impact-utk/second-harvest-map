d59d958780fffe72bb92c188c9f587855d20938a
Sai Thatigotla <sai.thatigotla@gmail.com>
2021-04-06 21:32:03 -0400

    ran linter #26

--- src/helpers/PhoneLink.ts
+++ src/helpers/PhoneLink.ts
@@ -3,22 +3,17 @@
  * @Param str The string to convert
  * @Returns A string telling what the input is
  */
- export default function ParsePhone(str: string): string {
-    let numb: string = str.replace(/\s+/g, '');
+export default function ParsePhone(str: string): string {
+  let numb: string = str.replace(/\s+/g, '');
 
-    let found = str.match(/^\(\d{3}\)\s\d{3}-\d{4}/g);
+  const found = str.match(/^\(\d{3}\)\s\d{3}-\d{4}/g);
 
-    if (found !== null)
-    {
-      numb = numb.replace(/(\(|\))/g, '-');
-      numb = numb.substring(1);
-      numb = "tel:" + numb;
+  if (found !== null) {
+    numb = numb.replace(/(\(|\))/g, '-');
+    numb = numb.substring(1);
+    numb = `tel:${numb}`;
 
     return numb;
-    }else
-    {
-      return 'error';
-    }
-    
   }
-  
+  return 'error';
+}
