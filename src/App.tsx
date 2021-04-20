a7d27ed8980901c1e09946069af381409d179c0b
huncfut <k.zeligowski@gmail.com>
2021-04-20 10:46:09 -0400

    Kubazeligowski/h4 ish 26 get food pantry data client (#28)
    * Fetching completed
    
    * Lint and types corrections
    
    * Update src/App.tsx
    
    Co-authored-by: Vishal Aiely <vaiely@vols.utk.edu>
    
    * First render only fix
    
    Co-authored-by: Vishal Aiely <vaiely@vols.utk.edu>
    
    Co-authored-by: Vishal Aiely <vaiely@vols.utk.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './styles/main.css';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import getFoodPantries from 'src/client/getFoodPantries';
+import { FoodPantry } from 'src/utils/types';
 import FoodPantryDisplay from './components/FoodPantryDisplay/index';
 import SearchButton from './components/SearchButton/index';
+import './styles/main.css';
+import './App.css';
 
 function App(): JSX.Element {
+  // Pass foodPantries to other components
+  const [foodPantries, setFoodPantries] = useState<FoodPantry[] | undefined>([]);
+
+  useEffect(() => {
+    const gatherData = async () => {
+      try {
+        setFoodPantries(await getFoodPantries());
+      } catch (e) {
+        console.log(`Error: ${e}`);
+      }
+    };
+
+    gatherData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
