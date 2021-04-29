8452e92c379787cebff988e994cae1653b068247
Alex Austin <raustin9@vols.utk.edu>
2021-04-29 16:13:33 -0400

    Style the map and image components for the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import CountyImage from 'src/components/infoandimage/countymapimage.png';
 import getFoodPantries from 'src/client/getFoodPantries';
 import { FoodPantry } from 'src/utils/types';
+import MapContainer from 'src/components/Map/index';
 import FoodPantryDisplay from './components/FoodPantryDisplay/index';
 import SearchButton from './components/SearchButton/index';
+
 import './styles/main.css';
 import './App.css';
 
@@ -28,7 +31,25 @@ function App(): JSX.Element {
     <div className="App">
       <header className="App-header">
         <SearchButton pantries={foodPantries} setFilteredPantries={setFilteredPantries} />
+        {/* <FoodPantryDisplay pantries={filteredPantries} /> */}
         <FoodPantryDisplay pantries={filteredPantries} />
+        <div className="MapArea">
+          <div className="divider" />
+          <h1 className="wordMap">Map</h1>
+          <div className="Map">
+            {/* <div className="MapContainer"> */}
+            <MapContainer pantries={foodPantries} />
+            {/* </div> */}
+            <div className="TextArea">
+              <p className="MapText">
+                Do you or someone you know need food assistance? Please call the pantries to find out their schedule of
+                distribution prior to going to the pantry. If you cannot find a food pantry in your county, please call
+                Second Harvest at (865) 521-0000.
+              </p>
+              <img src={CountyImage} alt="" className="CountyImage" />
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
